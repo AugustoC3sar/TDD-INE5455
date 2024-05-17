9bb7d6c8066b758192939528511d386987d6c5f6
Raquel Behrens <raquel.behrens@hotmail.com>
2024-05-17 19:56:39 -0300

    Cria teste para cadastrar funcionário

--- tests/testEmpresa.py
+++ tests/testEmpresa.py
@@ -6,17 +6,30 @@ import unittest
 from src.empresa import Empresa
 
 class TestEmpresa(unittest.TestCase):
-    def run_tests():
-        unittest.main()
+    def setUp(self):
+        self.empresa = Empresa()
 
+    '''
+        Teste 1
+    '''
     def test_criar_empresa(self):
-        # Implicit Fixture Setup
         # Inline Fixture Setup
         nome_empresa = 'Empresa 1'
         # Exercise SUT
         empresa = Empresa(nome_empresa)
         # Result Verification
         self.assertEqual(nome_empresa, empresa.nome)
+
+    '''
+        Teste 2
+    '''
+    def test_cadastrar_funcionario(self):
+        # Implicit Fixture Setup
+        # Exercise SUT
+        # Employee must have name, cpf, role and salary
+        self.empresa.cadastrarFuncionario('Fulano', '123.456.789-00', 'Gerente', 1000.0)
+        # Result Verification
+        self.assertEqual(1, len(self.empresa.funcionarios))
         
 if __name__ == '__main__':
     unittest.main()
