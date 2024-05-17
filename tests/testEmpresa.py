4b33a85170cb9b92535938645d8bc95d3430c358
Raquel Behrens <raquel.behrens@hotmail.com>
2024-05-17 20:13:56 -0300

    Adiciona teste de cadastrar funcionário com nome que não é string

--- tests/testEmpresa.py
+++ tests/testEmpresa.py
@@ -26,10 +26,18 @@ class TestEmpresa(unittest.TestCase):
     def test_cadastrar_funcionario(self):
         # Implicit Fixture Setup
         # Exercise SUT
-        # Employee must have name, cpf, role and salary
         self.empresa.cadastrarFuncionario('Fulano', '123.456.789-00', 'Gerente', 1000.0)
         # Result Verification
         self.assertEqual(1, len(self.empresa.funcionarios))
+
+    '''
+        Teste 3
+    '''
+    def test_cadastrar_funcionario_nome_invalido(self):
+        # Implicit Fixture Setup
+        # Exercise SUT
+        with self.assertRaises(ValueError):
+            self.empresa.cadastrarFuncionario(123, '12345678900', 'Gerente', 1000.0)
         
 if __name__ == '__main__':
     unittest.main()
