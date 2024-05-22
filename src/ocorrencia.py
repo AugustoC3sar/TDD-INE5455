3e4bc7afc3742401857643e25cc6e0827ddbe821
César <c.augusto0302@gmail.com>
2024-05-22 11:39:57 -0300

    Teste 22 ok

---
+++ src/ocorrencia.py
@@ -0,0 +1,35 @@
+from src.funcionario import Funcionario
+
+
+class Ocorrencia:
+    def __init__(self, id: int, resumo: str, responsavel: Funcionario, tipo: str, prioridade: int):
+        self.__id = id
+        self.__resumo = resumo
+        self.__responsavel = responsavel
+        self.__tipo = tipo
+        self.__prioridade = prioridade
+        self.__estado = 1
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def resumo(self):
+        return self.__resumo
+
+    @property
+    def responsavel(self):
+        return self.__responsavel
+    
+    @property
+    def tipo(self):
+        return self.__tipo
+    
+    @property
+    def prioridade(self):
+        return self.__prioridade
+    
+    @property
+    def estado(self):
+        return self.__estado
