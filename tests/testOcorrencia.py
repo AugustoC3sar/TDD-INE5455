6281b8a3edabc5f03dd96d343030472932c2d7b5
César <c.augusto0302@gmail.com>
2024-05-22 11:32:06 -0300

    Teste 22: criar ocorrencia

---
+++ tests/testOcorrencia.py
@@ -0,0 +1,29 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from src.ocorrencia import Ocorrencia
+from src.funcionario import Funcionario
+
+import unittest
+
+
+class TestOcorrencia:
+    def setUp(self):
+        self.responsavel = Funcionario("João", "145.689.023.45", "Analista de Redes", 3255.0)
+    
+    '''
+        Teste 22
+    '''
+    def test_criar_ocorrencia(self):
+        # Exercise SUT
+        ocorrencia = Ocorrencia(1, "Troca dos switches", self.responsavel, "tarefa", 3)
+
+        # Result Verification
+        self.assertEqual(ocorrencia.id, 1)
+        self.assertEqual(ocorrencia.resumo, "Troca dos switches")
+        self.assertEqual(ocorrencia.responsavel, self.responsavel)
+        self.assertEqual(ocorrencia.tipo, "tarefa")
+        self.assertEqual(ocorrencia.prioridade, 3)
+        self.assertEqual(ocorrencia.estado, 1)
+        
